ddc92f2786a48b047c4af47d52f74dc669284bd2
Manuel Gutiérrez <mg@bnext.es>
2020-11-13 13:14:44 +0100

    fix tests with new classes

--- gilded-rose-kata/test/gilded-rose.test.js
+++ gilded-rose-kata/test/gilded-rose.test.js
@@ -1,12 +1,15 @@
 const { Shop } = require('../src/shop')
 const { Item } = require('../src/item')
+const { AgedBrie } = require('../src/aged-brie')
+const { Pass } = require('../src/pass')
+const { Sulfuras } = require('../src/sulfuras')
 
 describe('Gilded Rose', function () {
   it('should decrease the regular item', function () {
     let items = [new Item('regular item', 1, 6)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('regular item', 0, 5)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('regular item')
     expect(itemUpdated.quality).toBe(5)
     expect(itemUpdated.sellIn).toBe(0)
@@ -16,7 +19,7 @@ describe('Gilded Rose', function () {
     let items = [new Item('regular item', 0, 6)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('regular item', -1, 4)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('regular item')
     expect(itemUpdated.quality).toBe(4)
     expect(itemUpdated.sellIn).toBe(-1)
@@ -26,77 +29,77 @@ describe('Gilded Rose', function () {
     let items = [new Item('regular item', 0, 1)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('regular item', -1, 0)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('regular item')
     expect(itemUpdated.quality).toBe(0)
     expect(itemUpdated.sellIn).toBe(-1)
   })
 
   it('should increase its quality', function () {
-    let items = [new Item('Aged Brie', 5, 4)]
+    let items = [new AgedBrie('Aged Brie', 5, 4)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Aged Brie', 4, 5)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Aged Brie')
     expect(itemUpdated.quality).toBe(5)
     expect(itemUpdated.sellIn).toBe(4)
   })
 
   it('should increase not increase the quality over 50', function () {
-    let items = [new Item('Aged Brie', 5, 50)]
+    let items = [new AgedBrie('Aged Brie', 5, 50)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Aged Brie', 4, 50)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Aged Brie')
     expect(itemUpdated.quality).toBe(50)
     expect(itemUpdated.sellIn).toBe(4)
   })
 
   it('should has sell in as undefined and quality never decrease', function () {
-    let items = [new Item('Sulfuras, Hand of Ragnaros', undefined, 80)]
+    let items = [new Sulfuras('Sulfuras, Hand of Ragnaros', undefined, 80)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Sulfuras, Hand of Ragnaros', undefined, 80)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Sulfuras, Hand of Ragnaros')
     expect(itemUpdated.quality).toBe(80)
     expect(itemUpdated.sellIn).toBe(undefined)
   })
 
   it('should increase its quality', function () {
-    let items = [new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10)]
+    let items = [new Pass('Backstage passes to a TAFKAL80ETC concert', 12, 10)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 11)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Backstage passes to a TAFKAL80ETC concert')
     expect(itemUpdated.quality).toBe(11)
-    expect(itemUpdated.sellIn).toBe(10)
+    expect(itemUpdated.sellIn).toBe(11)
   })
 
   it('should increase its quality twice', function () {
-    let items = [new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]
+    let items = [new Pass('Backstage passes to a TAFKAL80ETC concert', 11, 10)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 12)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Backstage passes to a TAFKAL80ETC concert')
     expect(itemUpdated.quality).toBe(12)
-    expect(itemUpdated.sellIn).toBe(9)
+    expect(itemUpdated.sellIn).toBe(10)
   })
 
   it('should increase its quality three times', function () {
-    let items = [new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)]
+    let items = [new Pass('Backstage passes to a TAFKAL80ETC concert', 5, 10)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 13)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Backstage passes to a TAFKAL80ETC concert')
     expect(itemUpdated.quality).toBe(13)
     expect(itemUpdated.sellIn).toBe(4)
   })
 
   it('should decrease its quality to zero', function () {
-    let items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
+    let items = [new Pass('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
     const gildedRose = new Shop(items)
     items = gildedRose.refreshInventory()
-    const itemUpdated = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0)
+    const itemUpdated = items[0]
     expect(itemUpdated.name).toBe('Backstage passes to a TAFKAL80ETC concert')
     expect(itemUpdated.quality).toBe(0)
     expect(itemUpdated.sellIn).toBe(-1)
